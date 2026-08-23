ce167ee748ca334226b36e822d4eb94f4f0ec61a
Iván Bejarano Chaine <a338729@uach.mx>
2026-08-23 00:15:07 -0600

    Inicio del proyecto, configuracion de la estructura del mismo utilizando un cliente + servidor

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-50'></div>
+    </>
+  )
+}
+
+export default App
